8dfd6f66cb78f4ea6b422abca0a6cf11a1b97976
cccccp <cccccp@hotmail.com>
2019-09-23 15:10:36 +0200

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -1,3 +1,15 @@
 def sum_two(array,k): #O(n) time/space complexity (search in a set is O(1) on average)
     s=set(k-x for x in array)
     return any(x in s for x in array)
+
+# If the numbers are supposed to be used accordingly to their counts in the array
+
+def sum_two(array,k): #O(n) time/space complexity (search in a set is O(1) on average)
+    s=set()
+    
+    for x in array:
+        if x in s:
+            return True
+        s.add(k-x)  
+    
+    return False
