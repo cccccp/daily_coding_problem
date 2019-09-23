d08f3c0bcb5f09bdc6e29fa81a9f34dd8a50396e
cccccp <cccccp@hotmail.com>
2019-09-23 15:49:43 +0200

    Update problem_4.py

--- problem_4.py
+++ problem_4.py
@@ -1,1 +1,12 @@
-
+def minimal_positive(array):
+    d=dict()
+    for x in array:
+        d[x]=0
+    
+    i=1
+    while True:
+        try:
+            d[i]
+        except KeyError:
+            return i
+        i+=1
