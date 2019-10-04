b28320ed24975c87bbbb637bb627f436556c62ed
cccccp <cccccp@hotmail.com>
2019-10-04 10:48:21 +0200

    Update problem_32.py

--- problem_32.py
+++ problem_32.py
@@ -1,4 +1,4 @@
 def arbitrage(currencies):
 	n=len(currencies[0])
-	return not(all((currencies[0][j]==currencies[0][i]*currencies[i][j] for i in range(n)) for j in range(n)))
+	return not(all(all(currencies[0][j]==currencies[0][i]*currencies[i][j] for i in range(n)) for j in range(n)))
   
