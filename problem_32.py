e64f1a8449b6da0edc799556a6c6eac6692d1b6a
cccccp <cccccp@hotmail.com>
2019-10-04 10:50:06 +0200

    Update problem_32.py

--- problem_32.py
+++ problem_32.py
@@ -1,4 +1,3 @@
-def arbitrage(currencies):
+def arbitrage(currencies): #O(n^2) optimal solution
 	n=len(currencies[0])
 	return not(all(all(currencies[0][j]==currencies[0][i]*currencies[i][j] for i in range(n)) for j in range(n)))
-  
