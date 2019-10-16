f0bd80ebc4fc0bf666161492dda726bf37d83bc8
cccccp <cccccp@hotmail.com>
2019-10-16 23:38:20 +0200

    Update problem_40.py

--- problem_40.py
+++ problem_40.py
@@ -1,3 +1,6 @@
+# If the number of repetitions is k=2, xor can be used. The general case follows when one notes that the xor operation is just the mod 2 bitwise addition.
+# It is then sufficient to perform the mod k digitwise addition in the general case. 
+
 def lonesome(A,k=3): #O(n) time complexity/ O(1) space complexity if the entries of A are bounded
     l=0
     for a in A:
