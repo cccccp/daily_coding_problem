c32c4769eb6108e7a672b9539021d771cd4f5b3a
cccccp <cccccp@hotmail.com>
2019-10-24 00:11:37 +0200

    Update problem_44.py

--- problem_44.py
+++ problem_44.py
@@ -1,22 +1,29 @@
 def inversions_merge(A1,A2): # auxilary function to merge 2 sorted lists and count the number of remaining inversions (O(max(n1,n2)) time complexity, O(n1+n2) space complexity)         
     n1, n2=len(A1), len(A2) 
-    A = A1 + A2
+    B=[]
     
     inv = 0
-    i, j = 0, n1          
+    # current A1-index for comparison
+    i = 0          
 
-    while j < n1+n2:
-        i=0
-        while A[i] < A[j]:
+    for j in range(n2):
+        while i<n1 and A1[i] < A2[j]:
+            B.append(A1[i])  
             i += 1
-        if i < j:
-            inv+=j-i
-            A[i+1:j+1]=A[i:j]
-            A[i]=A2[j-n1]
-        elif i==j:
-            j += 1
-    return inv, A
-    
+
+        if i <n1:
+            inv += n1-i
+        
+        B.append(A2[j])
+
+    # append the remaining elements of A1 to B (if any)
+    while i < n1:
+            B.append(A1[i])
+            i+=1
+
+    return inv, B
+
+# @trace
 def inversions(A): # O(nlog(n)) time/space complexity
     n=len(A)
     if n<2:
@@ -24,4 +31,4 @@ def inversions(A): # O(nlog(n)) time/space complexity
     inv1, A1 = inversions(A[:n//2])
     inv2, A2 = inversions(A[n//2:])
     inv, A = inversions_merge(A1, A2) 
-    return inv+inv1+inv2, A    
+    return inv+inv1+inv2, A 
