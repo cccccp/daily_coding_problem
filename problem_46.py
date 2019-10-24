10a2797330aa07e73a049eadf6404ea5a3be8821
cccccp <cccccp@hotmail.com>
2019-10-24 03:17:34 +0200

    Update problem_46.py

--- problem_46.py
+++ problem_46.py
@@ -1,14 +1,43 @@
-def longestpalindrome(char): # O(n^2) time complexity/ O(1) space complexity
-    n=len(char)
-    l=0
-    pal=''
-    for c in range(n):
-        i=0
-        while c-i >=0 and c+i<n and char[c-i]==char[c+i]:
-            if 2*i+1>l:
-                l=2*i+1
-                pal=char[c-i:c+i+1]
-            i+=1
-    return pal    
-
-# There also exists a linear time advanced algorithm due to Manacher (https://en.wikipedia.org/wiki/Longest_palindromic_substring)
+def longestpalindrome(char): # Manacher's algorithm: O(n) time/space complexity
+    # To avoid problems with even sized longest palindroms
+    s="^#"+"#".join(char)+"#$"
+    n=len(s)
+    
+    # definition of two auxilary parameters (right is the upper index of the previous longest palindrom; center is the corresponding position)
+    cen=0
+    right=0
+    
+    # array of the lengths of the longest palindroms centered at any position
+    p=[0]*n
+    
+    pal=""
+    length=0
+    
+    for i in range(1,n-1):
+        
+        # definition of a lower bound for the palindrom centered at i
+        
+        # Since (obviously) i+ min(p[mir],right-i)<=right, one has mir-min(p[mir],right-i)>=left (the symmetric of right, w.r.t. cen) 
+        # and the palindrom centered in mir of radius min(p[mir],right-i) is included in the first-half of the longest palindrom centered in cen.
+        # Hence p[i] >=min(p[mir],right-i) (the symmetric of any palindrom centered in mir, w.r.t. cen, is also a palindrom).
+        
+        mir=cen-(i-cen)
+        p[i]=max(0,min(p[mir],right-i))
+        
+        # determination of the length of the longest palindrom centered at i
+        while p[i]+1<=min(i,n-1-i) and s[i-p[i]-1]==s[i+p[i]+1]:
+            p[i]+=1
+        
+        if p[i]>length:
+            length=p[i]
+            pal=s[i-p[i]:i+p[i]+1]
+            
+        # new definition of the auxilary indexes
+        if i+p[i]>right:
+            right=i+p[i]
+            cen=i
+    
+    word_pal=[c for c in pal if c != "^" and c != "#" and c != "$"]
+    pal="".join(word_pal)
+    
+    return pal
