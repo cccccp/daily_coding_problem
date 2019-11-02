7c35946b114781eac88b1ada305436418733a599
cccccp <cccccp@hotmail.com>
2019-11-02 11:40:39 +0100

    Update and rename problem_58.py to problem_61.py

---
+++ problem_61.py
@@ -0,0 +1,11 @@
+def pow(x,n):
+    if n==0:
+        return 1
+    elif n==1:
+        return x
+    else:
+        y=pow(x,n//2)
+        if n%2==0:
+            return y*y
+        else:
+            return x*y*y
