ebe9ea7f68b3be3bead976630c4353df071576ad
cccccp <cccccp@hotmail.com>
2019-11-29 19:55:55 +0100

    Update problem_69.py

--- problem_69.py
+++ problem_69.py
@@ -1,2 +1,4 @@
 def maxproduct3(l):
-    return max(l[-1]*l[-2]*l[-3],l[0]*l[1*l[-1])
+    l.sort()
+    return max(l[-1]*l[-2]*l[-3],l[0]*l[1]*l[-1])
+# One can also avoid to sort the list (it is sufficient to get the two smallest values of l along with its three largest).
