468d8cabaa893bd94de15625137fccc8f2220738
cccccp <cccccp@hotmail.com>
2019-11-29 15:25:53 +0100

    Create problem_61.py

---
+++ problem_61.py
@@ -0,0 +1,14 @@
+#exponentiation rapide en anglais
+
+def square_and_multiply(x,n):
+	if n==0:
+		return 1
+	elif n==1:
+		return x
+	else:
+		m=n//2
+		y=square_and_multiply(x,m)
+		if m%2==0:
+			return y*y
+		else:
+			return y*y*x
